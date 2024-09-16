7a475ab76db4b2e7e5fe5f49adb8a85ddb470138
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2024-09-16 13:02:08 +0400

    Update Checking_account.py

--- Banking system/Checking_account.py
+++ Banking system/Checking_account.py
@@ -21,7 +21,7 @@ class CheckingAccount(Account):
             raise ValueError("Enter valid amount")
         
     def transfer(self, destination: Account, amount: float) -> None:
-        if amount >= 0 and isinstance(amount,float):
+        if isinstance(amount,float) and amount >= 0:
             if self._balance > amount:
                 self._balance -= amount
                 destination.deposit(amount)
